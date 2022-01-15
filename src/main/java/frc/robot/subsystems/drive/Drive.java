3a7e916f3dfc608f0550c93955dbd2a1fa147776
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-01-15 10:45:55 -0800

    Added drive subsystem

---
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Drive extends SubsystemBase {
+    private final CANSparkMax driveMotor;
+
+
+    public Drive(int driveMotorId) {
+        driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}
