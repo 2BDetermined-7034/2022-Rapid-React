68fdadcf8bfe35d1174bfeab35553d898f658874
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-01-15 10:07:47 -0800

    Set up constants, edit as needed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,14 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public final static class controllers {
+        public static final int gamePadPort = 0;
+        public static final int joystickPort = 1;
+        public static final int buttonPadPort = 2;
+    }
+    public final static class driveBase {
+        // Add NEO IDs here.
+    }
+}
