4b5d8d4eeb2a5e97440a33531101db62b81b43bb
Blakeley Nicholson <nicholsb@wlhs.wlwv.k12.or.us>
2022-01-17 09:37:26 -0800

    cleaned examplesubsystem from climb commands

--- src/main/java/frc/robot/commands/climb/RaiseClimber.java
+++ src/main/java/frc/robot/commands/climb/RaiseClimber.java
@@ -6,7 +6,6 @@ package frc.robot.commands.climb;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.TeleClimb;
 
 /** An example command that uses an example subsystem. */
